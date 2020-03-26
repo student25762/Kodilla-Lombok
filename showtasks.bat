8f1f145176883735a06f089ba52236eaa0f1d857
student25762 <hubertkrz@gmail.com>
2020-03-26 23:01:35 +0100

    task 21.4

--- showtasks.bat
+++ showtasks.bat
@@ -6,6 +6,10 @@ goto fail
 
 :openbrowser
 start http://localhost:8080/crud/v1/task/getTasks
+goto end
 
 :fail
 echo There are errors in your script or application
+
+:end
+echo Application is starting
