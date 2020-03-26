68687b713781ca0e4d23400d2af9f67b00846de7
student25762 <hubertkrz@gmail.com>
2020-03-26 22:16:20 +0100

    task 21.4

---
+++ showtasks.bat
@@ -0,0 +1,11 @@
+call runcrud.bat
+if "%ERRORLEVEL%" == "0" goto openbrowser
+echo.
+echo runcrud has errors - breaking work
+goto fail
+
+:openbrowser
+start http://localhost:8080/crud/v1/task/getTasks
+
+:fail
+echo There are errors in your script or application
